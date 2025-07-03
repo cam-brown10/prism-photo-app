8bd5ad2df5688e11b1525a957a21b53ec0083052
Cameron Brown <cameroninnapier@gmail.com>
2025-07-03 20:54:53 +1200

    Update models.py

--- submissions/models.py
+++ submissions/models.py
@@ -1,1 +1,10 @@
+from django.db import models
+
+class Submission(models.Model):
+    student_name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='submissions/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.student_name} - {self.image.name}"
 
